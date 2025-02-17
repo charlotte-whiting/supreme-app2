08373320807907e394f9c2d8413826042fa7c554
charlotte-whiting <cwhiting1104@gmail.com>
2025-02-16 17:59:49 -0800

    first-push!
    yippee

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
 import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [hello, setHello] = useState("Hello World");
+
+  useEffect(() => {
+    const makeCall = async () => {
+      try {
+        const res = await fetch("/api/hello");
+        if (!res.ok) {
+          throw new Error(`Response status: ${res.status}`);
+        }
+        const json = await res.json();
+        console.log(json);
+        setHello(JSON.stringify(json.message));
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    makeCall();
+  });
   return (
     <div>
-      Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
+      {hello} <Link href="/about">About</Link>
+      <Image
+        src={"/charlatte.jpeg"}
+        alt="charlatte car"
+        width="164"
+        height="164"
+      />
     </div>
   );
 }
