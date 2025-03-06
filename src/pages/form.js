373518d07eca17b99d63a22a84cc83e141b0173b
charlotte-whiting <cwhiting1104@gmail.com>
2025-03-06 14:53:40 -0800

    remove ids

--- src/pages/form.js
+++ src/pages/form.js
@@ -42,13 +42,13 @@ export default function FormPg() {
       <Stack container direction="column" sx={{maxWidth:1/2}} spacing={2}>
         <form onSubmit={onSubmit}>
           {/* name */}
-         <TextField label="name" variant="outlined" id="name" />
+         <TextField label="name" variant="outlined"/>
          {/* fingers? */}
-         <InputLabel id="finger">Approximate number of fingers</InputLabel>
-         <TextField  label="" variant="outlined" id="finger" />
+         <InputLabel>Approximate number of fingers</InputLabel>
+         <TextField  label="" variant="outlined"/>
           {/* how many hours of sleep */}
         {/* select all animals */}
-         <InputLabel id="animals">Pick animals that you resonate with:</InputLabel>
+         <InputLabel>Pick animals that you resonate with:</InputLabel>
          <Select value={animal} name="resonators" labelId="animals" multiple={true} onChange={onSelectChange}>
            <MenuItem value="capybara">capybara</MenuItem>
            <MenuItem value="skink">skink</MenuItem>
@@ -63,13 +63,13 @@ export default function FormPg() {
            <MenuItem value="barracuda">barracuda</MenuItem>
          </Select>
          {/* chatgpt prompt */}
-         <Typography id="prompt" >
+         <Typography>
           Please answer this definitely not AI generated prompt:{" "}
           {essayPrompt
             ? essayPrompt
             : "Oops no api key :/ ... just write something fun"}
          </Typography>
-         <TextField id="prompt" name="prompt" label="" rows="50" cols="50" />
+         <TextField name="prompt" label="" rows="50" cols="50" />
          <br/>
          <Button variant="outlined" type="submit">Submit Answers</Button>
        </form>
