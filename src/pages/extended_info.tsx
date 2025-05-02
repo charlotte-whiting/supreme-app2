edcbb50a36ed0bdbb507b70c61c9cdb70d21ba87
charlotte-whiting <cwhiting1104@gmail.com>
2025-05-01 21:27:46 -0700

    checkbox functionality

--- src/pages/extended_info.tsx
+++ src/pages/extended_info.tsx
@@ -12,24 +12,45 @@ import {
 import { useState } from "react";
 
 function SearchResults({ results }) {
-  const keys = Object.keys(results);
+  const [checkedMap, setCheckedMap] = useState({});
+  const categories = Object.keys(results);
+  categories.forEach((category) => {
+    checkedMap[category] = [];
+  });
+  const handleCheck = (category: string, name: string) => () => {
+    const shouldBeChecked = !checkedMap[category]?.name;
+    setCheckedMap({
+      ...checkedMap,
+      [category]: {
+        ...checkedMap[category],
+        [name]: shouldBeChecked,
+      },
+    });
+  };
+
   return (
     <Stack>
       <Typography>
         Here are the search results - select what interest you!
       </Typography>
       <Stack direction="row">
-        {keys.map((key) => {
-          console.log(results[key]);
+        {categories.map((category) => {
+          console.log(results[category]);
           return (
-            <FormControl key={key}>
-              <FormLabel>{key}</FormLabel>
+            <FormControl key={category}>
+              <FormLabel>{category}</FormLabel>
               <FormGroup>
-                {results[key].map((item, i) => {
+                {results[category].map((item, i) => {
                   return (
                     <FormControlLabel
                       key={i}
-                      control={<Checkbox checked={true} name={item.name} />}
+                      control={
+                        <Checkbox
+                          checked={checkedMap[category][item.name]}
+                          onChange={handleCheck(category, item.name)}
+                          name={item.name}
+                        />
+                      }
                       label={item.name}
                     />
                   );
@@ -59,7 +80,6 @@ export default function ExtendedInfo() {
     const aiResults = await response.json();
     setResults(JSON.parse(aiResults.message));
   };
-  console.log(results);
   return (
     <Stack>
       <TextField
