5806eab7333f0d2084f07e598d359b1261b0dba2
charlotte-whiting <97814150+charlotte-whiting@users.noreply.github.com>
2025-06-26 18:02:07 -0700

    cleaning up + example (#8)
    * cleaning up redundant code
    
    * Update comic_pricing.js
    
    * Update comic_pricing.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
-import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [hello, setHello] = useState("Hello World");
-
-  useEffect(() => {
-    const makeCall = async () => {
-      try {
-        const res = await fetch("/api/hello");
-        if (!res.ok) {
-          throw new Error(`Response status: ${res.status}`);
-        }
-        const json = await res.json();
-        setHello(JSON.stringify(json.message));
-      } catch (error) {
-        console.error(error.message);
-      }
-    };
-
-    makeCall();
-  });
   return (
     <div>
       <br />
